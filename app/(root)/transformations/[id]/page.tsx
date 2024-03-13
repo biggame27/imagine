d30289d0afdeca1a58d0e1df7c739e4e0847eeb5
biggame27 <koharrisonko@gmail.com>
2024-03-12 23:47:53 -0500

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      transformations page
+    </div>
+  )
+}
+
+export default TransformationsPage
+
+//localhost:3000/transformations/123
